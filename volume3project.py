21607e6c9224280ca397cab551763be1e85ee461
ojac <caelan.osman137@gmail.com>
2021-11-13 14:49:23 -0700

    data cleaning part 1

--- volume3project.py
+++ volume3project.py
@@ -9,7 +9,18 @@ def data_cleaning():
     data: pandas dataframe with the cleaned data
     '''
     flight_2016 = pd.read_csv('flight.csv', delimiter=',')
-    flight_2017 = pd.read_csv('fl_samp.csv', delimiter=',')
+    #drop useless flight data
+    flight_2016.drop(['Month', 'Year', 'Day', 'Flight_Date', 'FlightNum',
+                      'Departure_Time', 'DepDel15', 'Dep_Delay_Groups',
+                      'Arrival_Time', 'Arrival_Delay', 'Arr_Delay_Minutes',
+                      'Arr_Del_morethan15', 'Cancelled', 'Diverted',
+                      'DistanceGroup', 'Carrier_Delay', 'WeatherDelay', 'NAS_Delay',
+                      'Security_Delay', 'Late_Aircraft_Delay', 'Top_Carriers', 'Top_Origin',
+                      'DEPTIME_GROUP1', 'DEPTIME_GROUP2', 'DEPTIME_GROUP3' ], axis=1, inplace=True)
+
+    print(flight_2016.columns)
+    #flight_2017 = pd.read_csv('fl_samp.csv', delimiter=',')
 
 
     return
+data_cleaning()
